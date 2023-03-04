9f369938034a4d2d41ad3770e3954733d3cec669
Hamilton Mosquera <hammosquera@uan.edu.co>
2023-03-03 23:20:12 -0500

    communication with the api and creation of buttons in the user panel

--- src/pages/Admin/UsersAdmin.js
+++ src/pages/Admin/UsersAdmin.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { HeaderPage } from "../../components/Admin/HeaderPage";
+import { useUser } from "../../hooks";
 
 export function UsersAdmin() {
+  const { loading, users, getUsers } = useUser();
+  console.log(loading);
+  console.log(users);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
-    <div>
+    <>
+      <HeaderPage title="Usuarios" />
       <h1>estamos en User Admin</h1>
-    </div>
+    </>
   );
 }
